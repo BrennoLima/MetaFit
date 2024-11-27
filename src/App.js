e590a6bcd47a78dd9ef7f4d34207f782567e8543
Brenno Lima <brennocesar@live.com>
2024-11-27 08:40:05 -0500

    First steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoadingButton from '@mui/lab/LoadingButton';
+import Container from '@mui/material/Container';
+import OpenAI from 'openai';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [isLoading, setIsLoading] = useState(false);
+	const [diet, setDiet] = useState('');
+
+	const openai = new OpenAI({
+		apiKey: process.env.REACT_APP_OPENAI_API_KEY,
+		dangerouslyAllowBrowser: true,
+	});
+
+	const generateDiet = async () => {
+		setIsLoading(true);
+		const completion = await openai.chat.completions.create({
+			model: 'gpt-4o-mini',
+			messages: [
+				{
+					role: 'system',
+					content:
+						"You're an experienced nutritionist and you will create me a custom meal plan. Keeping in mind my wake up, sleep and exercise time.",
+				},
+				{
+					role: 'system',
+					content:
+						'Output should strictly follow this format: `{"dailySummary": { "calories": string; "protein": string; "carbs": string; "fats": string; }; "meals": { "name": string; "time": string; "content": {"name": string, "quantity": string}[]; "calories": string, "protein": string, "carbs": string, "fats": string }[];}` ONLY THE JSON OBJECT SHOULD BE RETURNED.',
+				},
+				{
+					role: 'system',
+					content:
+						'Age: 27, Gender: Male, Weight: 85kg, Height: 165cm, Goal: Fat loss, Activity: Moderate, Gym workout 5 times a week. Diet Preferences: None, Lifestyle and schedule: at least 5 meals a day, no preference to meal timing. Health considerations: No medical conditions. Supplements: Protein and vitamins',
+				},
+				{
+					role: 'system',
+					content:
+						'Adjust the diet to my day schedule: wake up at 5:30am, exercise from 6am to 7:30 am, sleep at 10pm. I should always have a meal before my workout',
+				},
+			],
+		});
+		setDiet(completion.choices[0].message.content);
+		console.log(JSON.parse(completion.choices[0].message.content));
+		setIsLoading(false);
+	};
+
+	return (
+		<Container>
+			<LoadingButton
+				loading={isLoading}
+				variant='contained'
+				onClick={generateDiet}
+			>
+				Generate diet
+			</LoadingButton>
+			{diet}
+		</Container>
+	);
 }
 
 export default App;
